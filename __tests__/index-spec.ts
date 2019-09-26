be230583fec1391030af8888a99e6e007a2146b4
Alex Gorischek <alex.gorischek@gmail.com>
2019-09-25 22:44:40 -0700

    Move test documents inline

--- __tests__/index-spec.ts
+++ __tests__/index-spec.ts
@@ -9,38 +9,38 @@ const numericKeyDoc = load('3.🙌');
 const twoPropertyDoc = load('4.🙌');
 
 test('Should lex an empty document', () => {
-  const lexed = lex(emptyDoc);
+  const lexed = lex('🙌✋');
   expect(lexed).toHaveLength(2);
 });
 
 test('Should lex a document with one property', () => {
-  const lexed = lex(onePropertyDoc);
+  const lexed = lex('🙌😃🐬✋');
   expect(lexed).toHaveLength(4);
 });
 
 test('Should lex a document with a multi-character property', () => {
-  const lexed = lex(multiCharacterPropertyDoc);
+  const lexed = lex('🙌😃🤤🐬🦓✋');
   expect(lexed).toHaveLength(4);
 });
 
 test('Should lex a document with a numeric key', () => {
-  const lexed = lex(numericKeyDoc);
+  const lexed = lex('🙌🐱😻🐜✋');
   expect(lexed).toHaveLength(4);
 });
 
 test('Should lex a document with multiple properties', () => {
-  const lexed = lex(twoPropertyDoc);
+  const lexed = lex('🙌🥳🌻🦘😻❤️✋');
   expect(lexed).toHaveLength(6);
 });
 
 test('Should error on an invalid character', () => {
   expect(() => {
-    lex(invalidDoc);
+    lex('💩');
   }).toThrow();
 });
 
 test('Should parse an empty document', () => {
-  const parsed = parse(emptyDoc);
+  const parsed = parse('🙌✋');
   expect(parsed).toHaveLength(2);
   expect(parsed[0].value).toBe('🙌');
   expect(parsed[1].value).toBe('✋');
