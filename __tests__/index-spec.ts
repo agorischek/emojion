d5a0e25debb03abd8451e4755ee060a1355a1436
Alex Gorischek <alex.gorischek@gmail.com>
2019-09-26 19:01:11 -0700

    Use Unicode encoding for lexing

--- __tests__/index-spec.ts
+++ __tests__/index-spec.ts
@@ -3,21 +3,30 @@ import { lex, parse } from '../src/index';
 test('Should lex an empty document', () => {
   const lexed = lex('🙌✋');
   expect(lexed).toHaveLength(2);
+  expect(lexed[0].text).toBe('🙌');
+  expect(lexed[1].text).toBe('✋');
 });
 
 test('Should lex a document with one property', () => {
-  const lexed = lex('🙌😃🐬✋');
+  const lexed = lex('🙌😃🕔✋');
   expect(lexed).toHaveLength(4);
+  expect(lexed[0].text).toBe('🙌');
+  expect(lexed[1].text).toBe('😃');
+  expect(lexed[2].text).toBe('🕔');
+  expect(lexed[3].text).toBe('✋');
 });
 
 test('Should lex a document with a multi-character property', () => {
   const lexed = lex('🙌😃🤤🐬🦓✋');
   expect(lexed).toHaveLength(4);
+  expect(lexed[1].text).toBe('😃🤤');
+  expect(lexed[2].text).toBe('🐬🦓');
 });
 
 test('Should lex a document with a numeric key', () => {
   const lexed = lex('🙌🐱😻🐜✋');
   expect(lexed).toHaveLength(4);
+  expect(lexed[2].text).toBe('🐜');
 });
 
 test('Should lex a document with multiple properties', () => {
@@ -42,11 +51,13 @@ test('Should parse a document with one property', () => {
   const parsed = parse('🙌😃🐬✋');
   expect(parsed).toHaveLength(3);
   expect(parsed[0].value).toBe('🙌');
+  expect(parsed[1][0][0][0].value).toBe('😃');
+  expect(parsed[1][0][0][1].value).toBe('🐬');
   expect(parsed[2].value).toBe('✋');
 });
 
 test('Should parse a document with two properties', () => {
-  const parsed = parse('🙌🥳🌻🦘😻❤️✋');
+  const parsed = parse('🙌🥳🌻🤤🕑✋');
   const properties = parsed[1][0][0];
   expect(properties).toHaveLength(2);
 });
--- modules/grammar.js
+++ modules/grammar.js
@@ -1,26 +1,67 @@
 // Generated automatically by nearley, version 2.19.0
 // http://github.com/Hardmath123/nearley
-(function () {
-function id(x) { return x[0]; }
+(function() {
+  function id(x) {
+    return x[0];
+  }
 
-  const lexer = require('./lexer').lexer
-var grammar = {
+  const lexer = require('../src/lexer').lexer;
+  var grammar = {
     Lexer: lexer,
     ParserRules: [
-    {"name": "document", "symbols": [(lexer.has("documentStart") ? {type: "documentStart"} : documentStart), (lexer.has("documentEnd") ? {type: "documentEnd"} : documentEnd)]},
-    {"name": "document", "symbols": [(lexer.has("documentStart") ? {type: "documentStart"} : documentStart), "content", (lexer.has("documentEnd") ? {type: "documentEnd"} : documentEnd)]},
-    {"name": "content$ebnf$1", "symbols": ["property"]},
-    {"name": "content$ebnf$1", "symbols": ["content$ebnf$1", "property"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
-    {"name": "content", "symbols": ["content$ebnf$1"]},
-    {"name": "property", "symbols": [(lexer.has("key") ? {type: "key"} : key), (lexer.has("value") ? {type: "value"} : value)]},
-    {"name": "property", "symbols": [(lexer.has("key") ? {type: "key"} : key), "object"]},
-    {"name": "object", "symbols": [(lexer.has("indent") ? {type: "indent"} : indent), "content", (lexer.has("outdent") ? {type: "outdent"} : outdent)]}
-]
-  , ParserStart: "document"
-}
-if (typeof module !== 'undefined'&& typeof module.exports !== 'undefined') {
-   module.exports = grammar;
-} else {
-   window.grammar = grammar;
-}
+      {
+        name: 'document',
+        symbols: [
+          lexer.has('documentStart')
+            ? { type: 'documentStart' }
+            : documentStart,
+          lexer.has('documentEnd') ? { type: 'documentEnd' } : documentEnd,
+        ],
+      },
+      {
+        name: 'document',
+        symbols: [
+          lexer.has('documentStart')
+            ? { type: 'documentStart' }
+            : documentStart,
+          'content',
+          lexer.has('documentEnd') ? { type: 'documentEnd' } : documentEnd,
+        ],
+      },
+      { name: 'content$ebnf$1', symbols: ['property'] },
+      {
+        name: 'content$ebnf$1',
+        symbols: ['content$ebnf$1', 'property'],
+        postprocess: function arrpush(d) {
+          return d[0].concat([d[1]]);
+        },
+      },
+      { name: 'content', symbols: ['content$ebnf$1'] },
+      {
+        name: 'property',
+        symbols: [
+          lexer.has('key') ? { type: 'key' } : key,
+          lexer.has('value') ? { type: 'value' } : value,
+        ],
+      },
+      {
+        name: 'property',
+        symbols: [lexer.has('key') ? { type: 'key' } : key, 'object'],
+      },
+      {
+        name: 'object',
+        symbols: [
+          lexer.has('indent') ? { type: 'indent' } : indent,
+          'content',
+          lexer.has('outdent') ? { type: 'outdent' } : outdent,
+        ],
+      },
+    ],
+    ParserStart: 'document',
+  };
+  if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = grammar;
+  } else {
+    window.grammar = grammar;
+  }
 })();
