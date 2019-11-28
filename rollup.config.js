6dfb4fbddec51c3997b53ac3b7a5a6aea577f2ab
Alex Gorischek <alex.gorischek@gmail.com>
2019-11-28 10:26:47 -0700

    Configure Rollup

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+import resolve from 'rollup-plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript';
+import commonjs from 'rollup-plugin-commonjs';
+
+const globals = {
+  lodash: 'lodash',
+};
+
+export default {
+  input: './src/index.ts',
+  output: {
+    file: 'bundle.js',
+    format: 'cjs',
+    globals: globals,
+  },
+  plugins: [
+    resolve(),
+    typescript(),
+    commonjs({
+      namedExports: {
+        'node_modules/lodash/lodash.js': [
+          'invert',
+          'merge',
+          'forEach',
+          'forOwn',
+        ],
+        'node_modules/moo/moo.js': ['compile'],
+        'node_modules/nearley/lib/nearley.js': ['Grammar', 'Parser'],
+      },
+    }),
+  ],
+};
