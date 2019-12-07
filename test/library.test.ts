68c583b0fb3f4dfb8a979885c393ea8e5a15257c
Alex Gorischek <alex.gorischek@gmail.com>
2019-12-06 21:20:03 -0800

    Add full-document tests

--- test/library.test.ts
+++ test/library.test.ts
@@ -5,11 +5,35 @@ const readJson = (filePath: string) => {
   return JSON.stringify(JSON.parse(fs.readFileSync(filePath, 'utf8')));
 };
 
+const readEmojion = (filePath: string) => {
+  return fs.readFileSync(filePath, 'utf8').trim();
+};
+
 describe('Library', () => {
-  test('should round-trip doc1', () => {
+  test('should round-trip doc1 (JSON)', () => {
     const json = readJson('./test/docs/1.json');
     const emojion = generate(json);
     const parsed = parse(emojion);
     expect(parsed).toEqual(json);
+  });
+  test('should round-trip doc2 (JSON)', () => {
+    const json = readJson('./test/docs/2.json');
+    const emojion = generate(json);
+    const parsed = parse(emojion);
+    expect(parsed).toEqual(json);
+  });
+  test('should round-trip doc3 (JSON)', () => {
+    const json = readJson('./test/docs/3.json');
+    const emojion = generate(json);
+    const parsed = parse(emojion);
+    fs.writeFileSync('out.sdlfjk', emojion);
+    expect(parsed).toEqual(json);
+  });
+
+  test('should round-trip docA (Emojion)', () => {
+    const emojion = readEmojion('./test/docs/A.🙌');
+    const json = parse(emojion);
+    const generated = generate(json);
+    expect(generated).toEqual(emojion);
   });
 });
